beeed550006fe16dd7c5db3268745caf24762f43
Gab-Sinclair <gabrielle23@knights.ucf.edu>
2020-01-16 10:52:18 -0500

    new commit

--- lab1/index.js
+++ lab1/index.js
@@ -1,4 +1,5 @@
 const Dan = require("./dan.js");
    
 let p = new Dan("Dan", "green");
-  console.log(p.name);
+
+  p.speak();
--- lab1/person.js
+++ lab1/person.js
@@ -1,8 +1,13 @@
+ const chalk = require("chalk");
+
 class person {
         constructor(name,color){
             this.name = name;
             this.color =color;
         }
 
+        speak(){
+            console.log(chalk.keyword(this.favoriteColor)(this.name));
+        }
 }
 module.exports = person; 
