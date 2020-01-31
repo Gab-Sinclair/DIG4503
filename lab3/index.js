9611967cf7bb8419429192c3dc0cee323647f7ae
Gabrielle Sinclair <you@example.com>
2020-01-30 23:28:41 -0500

    new commit

--- lab3/index.js
+++ lab3/index.js
@@ -1,21 +1,13 @@
 const Express = require("express");
 const App = Express();
 const port = 80;
-const Person = require("./person.js");
 
-let p = new Person("Gabby","Green");
-
-App.use(Express.static("static"));
-
-App.get("/public", function(request, res){
-   res.send(p.getName());
-});
+App.use(Express.static("public"));
 
 App.get("/", function(request, res){
    console.log("got a request");
    res.send("Hello, world!");
 });
-
 
 App.listen(port, function(){
     console.log("server running");
