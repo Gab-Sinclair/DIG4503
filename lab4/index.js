8eee2a7dc9132e8af6603e07789acc1c386a563e
Gabrielle Sinclair <you@example.com>
2020-02-03 20:12:43 -0500

    new

--- lab4/index.js
+++ lab4/index.js
@@ -2,13 +2,20 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 
+//request id from id file send id 
+App.use("/id/#id", Express.static(""));
 
+//request name from name file send name file 
+App.use("/name/#name", Express.static(""));
+  
 
+//console log when request is made on the server 
 App.get("/", function(req, res){
     console.log("Got a request");
     res.send("");
  });
 
+ //listen on port 80 
 App.listen(port, function(){
     console.log("server running");
    } 
