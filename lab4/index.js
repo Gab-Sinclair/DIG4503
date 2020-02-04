757070761d40b75ca679c0317fb68c4db17bbd12
Gabrielle Sinclair <you@example.com>
2020-02-03 20:15:38 -0500

    new commit

--- lab4/index.js
+++ lab4/index.js
@@ -5,14 +5,14 @@ const port = 80;
 //request id from id file 
 App.use("/id/#id", Express.static(""));
      //send json object with matching id  
-
+     //show error if id does not exit
 
 
 //request name from name file  
 App.use("/name/#name", Express.static(""));
     //send json object qith matching name
+    //show error is name does not exit
 
-    
 
 //console log when request is made on the server 
 App.get("/", function(req, res){
