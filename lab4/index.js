209da12b7b6c6327d8586210dac6caf9dfd0f1ff
Gabrielle Sinclair <you@example.com>
2020-02-08 22:06:20 -0500

    new commit

--- lab4/index.js
+++ lab4/index.js
@@ -10,11 +10,12 @@ App.get("/id/:id", function(req, res){
     pokemon.forEach((value)=>{
         if(value.id ==req.params.id){
             res.send(req.params);
+            console.log(chalk.green(req.path));
         }
 
 });
 res.send("invalid");
-console.log(chalk.red.req.originalUrl);
+console.log(chalk.red(req.path));
 });
 
 
@@ -24,12 +25,12 @@ App.get("/name/:name", function(req, res){
     pokemon.forEach((value)=>{
         if(value.name ==req.params.name){
             res.send(req.params);
-        
+            console.log(chalk.green(req.path));
         }
 
 });
 res.send("invalid");
-console.log(chalk.red.params.path);
+console.log(chalk.red(req.path));
 });
 
 
