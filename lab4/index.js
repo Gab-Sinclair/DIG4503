c5a9ade05173a4f2cf2e142255208c284889212f
Gabrielle Sinclair <you@example.com>
2020-02-13 00:05:36 -0500

    new

--- lab4/index.js
+++ lab4/index.js
@@ -4,19 +4,28 @@ const port = 80;
 const pokemon = require('json-pokemon');
 const chalk = require("chalk");
 
-let result = {"error":"404 thing not found"};
 
 
 //find id in pokemon array
 App.get("/id/:id", function(req, res){
+    let result = {"error":"404 thing not found"};
+
     pokemon.forEach((value)=>{
-        if(value.id ==req.params.id){
-            result = {value};
+        if(value.id == req.params.id){
+            result = value;
         }
-        else{
-            console.log(chalk.red(req.path));
-        }
+
     });
+    
+    if(result.error){
+        console.log(chalk.red(req.path));
+     }
+     
+     else{
+         console.log(chalk.green(req.path));
+         res.send(result);
+     }
+     
 
 });
 
@@ -24,30 +33,32 @@ App.get("/id/:id", function(req, res){
 
 //find name in pokemon array
 App.get("/name/:name", function(req, res){
-    pokemon.forEach((value)=>{
-        if(value.name ==req.params.name){
-            result = {value};
-        }
-    });
-
-        if(result == result.error){
-            console.log(chalk.red(req.path));
-        }
-
-        else{
-            console.log(chalk.green(req.path));
-            res.send(result);
-        }
-
+    let result = {"error":"404 thing not found"};
     
+    pokemon.forEach((value)=>{
+        if(value.name == req.params.name){
+            result = value;
+        }
     });
+   
+   console.log(result);
 
+    if(result.error){
+       console.log(chalk.red(req.path));
+    }
+    
+    else{
+        console.log(chalk.green(req.path));
+        res.send(result);
+    }
+    
+    
+});
 
 
 //console log when request is made on the server 
 App.get("/", function(req, res){
     console.log("Got a request");
-    console.log(chalk.green(req.path));
     res.send("");
  });
 
