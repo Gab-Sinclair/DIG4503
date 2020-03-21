70c189a966cd4c7061a0d91488777dabd74f888e
Gabrielle Sinclair <you@example.com>
2020-03-20 23:50:58 -0400

    updating coponenets

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,5 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import IdSearch from '../components/IdSearch';
+import NameSearch from '../components/NameSearch';
 
+class Home extends React.Component {
+    render(){
+    return(
+        <>
+        <IdSearch/>
+        <NameSearch/>
+        </>
+    
+    )
+  }
+}
 
 
 export default Home
